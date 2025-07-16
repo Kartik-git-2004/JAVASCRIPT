4bde1e48e49a169f80d89f15be50287489d96899
Kartik-git-2004 <kartiksharmapmsss@gmail.com>
2025-07-16 03:10:18 +0000

    on

--- basics/ibjects_indepth.js
+++ basics/ibjects_indepth.js
@@ -1,13 +1,13 @@
-// const newUser = new Object();
-// newUser = {
-//     email: "kartik@google.com",
-//     fullname: {
-//         userFullname: {
-//             firstname: "kartik",
-//             secondname: "sharma"
-//         }
-//     }
-// }
+let newUser = new Object();
+newUser = {
+    email: "kartik@google.com",
+    fullname: {
+        userFullname: {
+            firstname: "kartik",
+            secondname: "sharma"
+        }
+    }
+}
 // Singelton  design  patter  or singelton object 
 
 // const user = (function () {
@@ -50,3 +50,15 @@ const obj2 = { 4: "t", 5: "i", 6: "k" };
 const obj3= Object.assign(obj1,obj2);
 console.log(obj3);
 
+// object as an array element
+
+const arr = [{1:"kartik",location:"jammu"},{2:"sharma"}];
+
+console.log(arr);
+console.log(arr[0]);
+console.log(arr[1]);
+
+
+console.log(Object.keys(newUser));
+console.log(Object.values(newUser));
+console.log(Object.entries(newUser)); 
